20e875df3f68e67cde1bd624a2d12104617fc643
plapicola <p.lapicola@gmail.com>
2019-05-02 14:39:18 -0600

    Adds location model and initial implementation of route for creating favorites

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var fetch = require('node-fetch');
 const pry = require('pryjs');
 var User = require('../../../models').User;
+var Location = require('../../../models').Location;
+var Favorite = require('../../../models').Favorite;
 
 /* POST new favorite */
 router.post('/', function(req, res) {
@@ -22,6 +24,24 @@ router.post('/', function(req, res) {
     foundUser = user;
     return findOrCreateCity(req.body.location);
   })
+  .then(city => {
+    eval(pry.it);
+    locationName = city.name
+    return Favorite.create({
+      UserId: foundUser.id,
+      CityId: city.id
+    })
+  })
+  .then(() => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).send(JSON.stringify({message: `${locationName} has been added to your favorites.`}));
+  })
+  .catch(error => {
+    console.log(error)
+    eval(pry.it);
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send(JSON.stringify({message: "Unable to add location to favorites"}));
+  })
 })
 
 function findOrCreateCity(location) {
@@ -31,24 +51,31 @@ function findOrCreateCity(location) {
         name: location
       }
     })
-    .then(foundLocation => {
+    .then(function(foundLocation) {
       if (foundLocation) {
         resolve(foundLocation);
       } else {
         return fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_GEOCODE_KEY}&address=${location}`)
       }
     })
-    .then(request => JSON.parse(data).results[0].geometry.location)
+    .then(request => {
+      return request.json();
+    })
+    .then(result => {
+      return result.results[0].geometry.location
+    })
     .then(response => {
-      eval(pry.it);
       return Location.create({
         name: location,
         latitude: response.lat,
-        latitude: response.lng
+        longitude: response.lng
       })
     })
     .then(function(location) {
       resolve(location)
+    })
+    .catch(function(error) {
+      reject()
     })
   })
 }
