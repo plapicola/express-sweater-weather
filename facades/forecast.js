dd11a323a84fdedff4f2e39422f22b56a28b2f13
plapicola <p.lapicola@gmail.com>
2019-05-05 11:01:41 -0600

    Adds documentation per feedback from @aprildagonese. Removes console.logs from facade

--- facades/forecast.js
+++ facades/forecast.js
@@ -3,28 +3,28 @@ var GeocodeService = require('../services/geocode');
 var ForecastService = require('../services/forecast');
 
 module.exports = class ForecastFacade {
+  /* Facade object contains the status and body of the response,
+     which is initialized in the static method for the endpoint */
   constructor(status, body) {
     this.status = status
     this.body = body
   }
 
   static forecastForUser(api_key, location) {
-    console.log(location);
     return new Promise((resolve, reject) => {
       User.authenticate(api_key)
       .then(function() {
         return GeocodeService.requestLocation(location)
       })
       .then(function(location) {
-        console.log(location);
         return ForecastService.requestForecast(location)
       })
       .then(function(forecast) {
         delete forecast.minutely;
-        resolve(new ForecastFacade(200, forecast));
+        resolve(new ForecastFacade(200, forecast)); // Got valid forecast, send 200
       })
       .catch(error => {
-        resolve(new ForecastFacade(401, error));
+        resolve(new ForecastFacade(401, error)); // Bad request, send 401 and error
       })
     })
   }
