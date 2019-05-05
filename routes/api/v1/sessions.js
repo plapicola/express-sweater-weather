ac553916a0b6c9234e6142476ad825fc8ce0159d
plapicola <p.lapicola@gmail.com>
2019-05-05 11:46:55 -0600

    Updates sessions api endpoint to use facade

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -1,29 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../../../models').User;
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+var SessionsCreateFacade = require('../../../facades/sessions_create');
 
 router.post('/', function(req, res) {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
+  res.setHeader("Content-Type", "application/json");
+  SessionsCreateFacade.authenticateUser(req.body.email, req.body.password)
+  .then(facade => {
+    res.status(facade.status).send(facade.body);
   })
-  .then(user => {
-    bcrypt.compare(req.body.password, user.password_digest, function(err, match) {
-      if (match) {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).send(JSON.stringify({api_key: user.api_key}));
-      } else {
-        res.setHeader("Content-Type", "application/json");
-        res.status(401).send(JSON.stringify({error: "Invalid email or password"}));
-      }
-    })
-  })
-  .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(401).send(JSON.stringify({error: "Invalid email or password"}));
+  .catch(facade => {
+    res.status(facade.status).send(facade.body);
   })
 });
 
