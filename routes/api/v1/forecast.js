1debacbce3e98d6b39823d547182ec21def96435
plapicola <p.lapicola@gmail.com>
2019-05-05 10:27:08 -0600

    Refctors routes to use forecast facade

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,27 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../../../models').User;
-var GeocodeService = require('../../../services/geocode');
-var ForecastService = require('../../../services/forecast');
-require('dotenv').config(); // Loads environment variables from .env file
+var ForecastFacade = require('../../../facades/forecast');
 
 /* GET forecast for a city */
 router.get('/', function(req, res) {
-  User.authenticate(req.body.api_key)
-  .then(function() {
-    return GeocodeService.requestLocation(req.query.location)
-  })
-  .then(function(location) {
-    return ForecastService.requestForecast(location)
-  })
-  .then(function(forecast) {
-    delete forecast.minutely;
+  ForecastFacade.forecastForUser(req.body.api_key, req.query.location)
+  .then(forecast => {
     res.setHeader("Content-Type", "application/json");
-    res.status(200).send(JSON.stringify(forecast));
-  })
-  .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(401).send(JSON.stringify({error: error}))
+    res.status(forecast.status).send(forecast.body);
   })
 })
 
